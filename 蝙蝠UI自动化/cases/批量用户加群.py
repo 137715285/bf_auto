43e45b71bf05c99b2d7cbb5e005efa6a84948d7b
buy now <63436569+137715285@users.noreply.github.com>
2020-05-08 00:39:18 +0800

    Add files via upload

--- 蝙蝠UI自动化/cases/批量用户加群.py
+++ 蝙蝠UI自动化/cases/批量用户加群.py
@@ -5,7 +5,7 @@ import time
 
 desired_caps = {
   'platformName': 'Android', # 被测手机是安卓
-  'platformVersion': '6', # 手机安卓版本
+  'platformVersion': '8', # 手机安卓版本
   'deviceName': 'xxx', # 设备名，安卓手机可以随意填写
   'appPackage': 'com.woyue.batchat', # 启动APP Package名称
   'appActivity': '.act.SplashActivity', # 启动Activity名称
@@ -31,22 +31,53 @@ driver.find_element_by_id("tvLoginById1").click()
 # # 定位账号输入框
 # mm = driver.find_elements_by_id("et")
 # print(mm)
+
+def th_dly():
+    # 在首页，退出到登录页面
+    wd = driver.find_element_by_android_uiautomator(
+        'new UiSelector().text("我的")'
+    )
+    wd.click()
+    # 点击个人设置
+    w = driver.find_element_by_id("tvBatId")
+    w.click()
+    # 点击退出登录
+    w = driver.find_element_by_id("btnExit")
+    w.click()
+    # 点击确定
+    w = driver.find_element_by_id("confirm")
+    w.click()
+
+
+def login_sy():
+    # 点击 登录 按钮
+    w = driver.find_element_by_id("btnLogin")
+    w.click()
+    # 点击 使用bat id登录
+    w = driver.find_element_by_id("tvLoginById1")
+    w.click()
+
+
 # 读取excel
-book = load_workbook(rf"C:\\Users\\Administrator\\Desktop\\jhy.xlsm")
+book = load_workbook(rf"C:\\Users\\13771\\Desktop\\蝙蝠UI自动化\\cases\\jhy.xlsm")
 sheet = book.active
 vals = []
 for cell in sheet['A']:
     vals.append(cell.value)
-doc = open('out.txt', 'w')
+
+
 
  # 循环登录读取的账号
-for val in vals[5:1001]:
+for val in vals[20:190]:
+    # 定位 账号输入框，并输入账号
     zh = driver.find_element_by_android_uiautomator(
         'new UiSelector().text("请输入Bat ID")'
     )
     zh.send_keys(val)
+    doc = open('out.txt', 'w')
     print(val)
     print(val, file=doc)
+    # 定位 密码输入框，并输入密码
     ele = driver.find_element_by_xpath(rf'//android.view.ViewGroup[3]/android.view.ViewGroup/android.widget.EditText')
     ele.send_keys('1234567q')
     # 点击登录按钮
@@ -62,35 +93,35 @@ for val in vals[5:1001]:
     # 点击输入框
     sr = driver.find_element_by_id("tvSearch")
     sr.click()
-    # 跳转页面再点击输入框
+    # 跳转页面定位输入框输入群号
     sr = driver.find_element_by_id("inputView")
     sr.send_keys('395360')
     # 点击找群
-    driver.find_element_by_id("tvUserHint").click()
+    q = driver.find_element_by_id("tvGroupHint")
+    q.click()
+    # 点击搜索结果群
+    q = driver.find_element_by_id("tvNum")
+    q.click()
 
+    #yl = driver.find_element_by_id("tvSendMessage").is_displayed()
+    try:
+        driver.find_element_by_id("tvSendMessage").click()
+        driver.keyevent(4)
+        time.sleep(1)
+        th_dly()
+        login_sy()
 
-    # 返回首页
-    driver.keyevent(4)
-    # 点击我的
-    wd = driver.find_element_by_android_uiautomator(
-        'new UiSelector().text("我的")'
-    )
-    wd.click()
-    # 点击个人设置
-    w = driver.find_element_by_id("tvBatId")
-    w.click()
-    # 点击退出登录
-    w = driver.find_element_by_id("btnExit")
-    w.click()
-    # 点击确定
-    w = driver.find_element_by_id("confirm")
-    w.click()
+    except :
 
-    # 账号登录
-    w = driver.find_element_by_id("btnLogin")
-    w.click()
-    # 点击使用 batid登录
-    w = driver.find_element_by_id("tvLoginById1")
-    w.click()
+        # 点击申请加群
+        q = driver.find_element_by_id("tvApplyGroup")
+        q.click()
+        time.sleep(1)
+
+        # 返回首页
+        driver.keyevent(4)
+        time.sleep(1)
+        th_dly()
+        login_sy()
 
 pass
